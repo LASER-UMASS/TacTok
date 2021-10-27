30107189e071484e455e3106fbdd2187c5d834e7
Talia Ringer <talia@dependenttyp.es>
2021-10-27 12:29:09 -0500

    Update unknown splitting

--- TacTok/split_unks.sh
+++ TacTok/split_unks.sh
@@ -5,10 +5,16 @@ cd names
 function split_unk_thresh {
   python ../split_unk.py names.pickle --threshold $1
   python ../split_unk.py locals.pickle --threshold $1
+  python ../split_unk.py paths.pickle --threshold $1
+  python ../split_unk.py merged.pickle --threshold $1
   mv names-known.pickle names-known-$1.pickle
   mv names-unknown.pickle names-unknown-$1.pickle
   mv locals-known.pickle locals-known-$1.pickle
   mv locals-unknown.pickle locals-unknown-$1.pickle
+  mv paths-known.pickle paths-known-$1.pickle
+  mv paths-unknown.pickle paths-unknown-$1.pickle
+  mv merged-known.pickle merged-known-$1.pickle
+  mv merged-unknown.pickle merged-unknown-$1.pickle
 }
 
 threshold=1000
