a37fd649b57a03eb676bbaa90e60b61bab669c42
Alex Sanchez-Stern <sanchezstern@cs.umass.edu>
2021-10-28 13:44:52 -0400

    Script cleanup

--- jobs/evaluate_proj.sh
+++ jobs/evaluate_proj.sh
@@ -2,17 +2,17 @@
 
 set -e
 
+TT_DIR=$HOME/work/TacTok
+
 echo "Evaluating project $1 on ${HOST}"
 $HOME/opam-scripts/read-opam.sh
 opam switch "4.07.1+flambda"
 eval $(opam env)
 
-cd work/TacTok
+cd $TT_DIR
 COQ_ROOT=$(pwd)/coq
 COQBIN=$COQ_ROOT/bin/
 export PATH=$COQBIN:$PATH
-
-sertop --version
 
 cd TacTok
 python evaluate.py ours tactok_results --path runs/tok/checkpoints/model_003.pth "$@"
