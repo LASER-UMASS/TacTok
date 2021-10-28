96f6a6725f936a7dcc36fec18fe955c874fb7bda
Alex Sanchez-Stern <sanchezstern@cs.umass.edu>
2021-10-28 13:45:34 -0400

    Fix some possible crashes in the evaluate parallel script

--- evaluate-proj-parallel.sh
+++ evaluate-proj-parallel.sh
@@ -4,7 +4,9 @@ TT_DIR=$HOME/work/TacTok
 PROJ=$(jq -r ".projs_test[]" ${TT_DIR}/projs_split.json | awk "NR==($1+1)")
 NUM_FILES=$(find ${TT_DIR}/data/${PROJ} -name "*.json" | wc -l)
 
-for file_idx in $(eval echo "{1..$NUM_FILES}"); do
-  sbatch -p longq --output=output/evaluate/evaluate_proj_${1}_${file_idx}.out --mem=5G \
-    jobs/evaluate_proj_tactok.sh --proj_idx $1 --file_idx ${file_idx} --output_dir=bpe_evaluation
+mkdir -p output/evaluate
+
+for file_idx in $(eval echo "{0..$(($NUM_FILES - 1))}"); do
+  sbatch -p longq --output=output/evaluate/evaluate_proj_${1}_${file_idx}.out \
+    jobs/evaluate_proj.sh --proj_idx $1 --file_idx ${file_idx} --output_dir=bpe_evaluation
 done
