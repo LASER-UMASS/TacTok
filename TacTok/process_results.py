35f8f6a6ec5959f7870a395038f4b7e29ae1416e
Alex Sanchez-Stern <sanchezstern@cs.umass.edu>
2021-10-29 13:45:52 -0400

    Take results dir as argument to process results

--- TacTok/process_results.py
+++ TacTok/process_results.py
@@ -6,6 +6,7 @@ import numpy as np
 import pickle
 import random
 import itertools 
+import argparse
 
 projs = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,16,17,18,19,20,21,22,23,24,25,26]
 
@@ -37,7 +38,11 @@ def combine(results_dict):
 def get_success_rate(results_set, num_theorems):
 	return (float(len(results_set))/float(num_theorems))*100
 
-results_dict, num_theorems = get_results_dict("bpe_evaluation/tactok_results") #replace with dirname
-results_set = combine(results_dict)
-success_rate = get_success_rate(results_set, num_theorems) 
-print(f"{len(results_set)}/{num_theorems} ({success_rate:.2f}%)")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('results_dir')
+    args = parser.parse_args()
+    results_dict, num_theorems = get_results_dict(args.results_dir)
+    results_set = combine(results_dict)
+    success_rate = get_success_rate(results_set, num_theorems)
+    print(f"{len(results_set)}/{num_theorems} ({success_rate:.2f}%)")
