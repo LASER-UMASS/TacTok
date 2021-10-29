dc88357f5f5e995ccef3418b291622bde9dd199d
Alex Sanchez-Stern <sanchezstern@cs.umass.edu>
2021-10-28 22:05:05 -0400

    Add individual proof support to find_missing_outputs.sh, refactor into a machine-readable subscript

--- jobs/find_missing_outputs.sh
+++ jobs/find_missing_outputs.sh
@@ -6,17 +6,6 @@ set -e
 
 TT_DIR=$HOME/work/TacTok
 
-OUTPUT_DIR=$1
-NUM_PROJS=$(jq ".projs_test[]" ${TT_DIR}/projs_split.json | wc -l)
-for proj_idx in $(eval echo "{0..$(($NUM_PROJS - 1))}"); do
-  if [[ -f ${OUTPUT_DIR}/results_${proj_idx}.json ]] ; then
-    continue
-  fi
-  PROJ=$(jq -r ".projs_test[]" ${TT_DIR}/projs_split.json | awk "NR==($proj_idx+1)")
-  NUM_FILES=$(find ${TT_DIR}/data/${PROJ} -name "*.json" | wc -l)
-  for file_idx in $(eval echo "{0..$((NUM_FILES - 1))}"); do
-    if [[ ! -f ${OUTPUT_DIR}/results_${proj_idx}_${file_idx}.json ]] ; then
-      echo "In project ${proj_idx} (${PROJ}), output for file ${file_idx} could not be found"
-    fi
-  done
-done
+$TT_DIR/jobs/find_missing_outputs_csv.sh $@ | awk -F, \
+  '{if ($5=="") {print "In project "$1" ("$2"), output for file "$3" ("$4") could not be found"} \
+    else        {print "In project "$1" ("$2"), in file "$3" ("$4"), proof "$5" ("$6") could not be found"}}'
---
+++ jobs/find_missing_outputs_csv.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -e
+shopt -s nullglob
+
+[[ "$#" -ne 1 ]] && echo "Wrong number of parameters! This script takes one argument, a directory to search" && exit 1
+
+TT_DIR=$HOME/work/TacTok
+
+OUTPUT_DIR=$1
+NUM_PROJS=$(jq ".projs_test[]" ${TT_DIR}/projs_split.json | wc -l)
+for proj_idx in $(eval echo "{0..$(($NUM_PROJS - 1))}"); do
+  if [[ -f ${OUTPUT_DIR}/results_${proj_idx}.json ]] ; then
+    continue
+  fi
+  PROJ=$(jq -r ".projs_test[]" ${TT_DIR}/projs_split.json | awk "NR==($proj_idx+1)")
+  NUM_FILES=$(find ${TT_DIR}/data/${PROJ} -name "*.json" | wc -l)
+  for file_idx in $(eval echo "{0..$((NUM_FILES - 1))}"); do
+    if [[ ! -f ${OUTPUT_DIR}/results_${proj_idx}_${file_idx}.json ]] ; then
+      SOME_PROOF_OUTPUT=false
+      potential_matches=(${OUTPUT_DIR}/results_${proj_idx}_${file_idx}_*.json)
+      if (( ${#potential_matches[@]} )); then
+        PROOFS=$(python print_proof_names.py --proj=${PROJ} --file_idx=${file_idx})
+        NUM_PROOFS=$(echo "$PROOFS" | wc -l)
+        for proof_idx in $(eval echo "{0..$(($NUM_PROOFS - 1))}"); do
+          PROOF=$(echo "$PROOFS" | awk "NR==($proof_idx+1)")
+          if [[ -f ${OUTPUT_DIR}/results_${proj_idx}_${file_idx}_${PROOF}.json ]] ; then
+            SOME_PROOF_OUTPUT=true
+            break
+          fi
+        done
+      fi
+      FILE=$(cd data/${PROJ} && find . -name "*.json" | awk "NR==($file_idx+1)")
+      if [ "$SOME_PROOF_OUTPUT" = true ] ; then
+        for proof_idx in $(eval echo "{0..$(($NUM_PROOFS - 1))}"); do
+          PROOF=$(echo "$PROOFS" | awk "NR==($proof_idx+1)")
+          if [[ ! -f ${OUTPUT_DIR}/results_${proj_idx}_${file_idx}_${PROOF}.json ]] ; then
+            echo "${proj_idx},${PROJ},${file_idx},${FILE},${proof_idx},${PROOF}"
+          fi
+        done
+      else
+        echo "${proj_idx},${PROJ},${file_idx},${FILE},,"
+      fi
+    fi
+  done
+done
