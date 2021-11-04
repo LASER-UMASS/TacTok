809ae60520e795665cb0a5304c95c227b598ad19
Alex Sanchez-Stern <sanchezstern@cs.umass.edu>
2021-11-04 18:48:17 -0400

    Fix output directory in rerun missing files

--- swarm/rerun-missing-files.sh
+++ swarm/rerun-missing-files.sh
@@ -8,11 +8,13 @@ TT_DIR=$HOME/work/TacTok
 EVAL_ID=$1
 shift 1
 
+mkdir -p output/evaluate/${EVAL_ID}
+
 $TT_DIR/swarm/find-missing-outputs-csv.sh ${TT_DIR}/TacTok/evaluation/${EVAL_ID} |
 while IFS=, read -r proj_idx proj_name file_idx file_name proof_idx proof_name; do
   if [[ proof_idx != "" ]]; then
       sbatch -p longq -J ${EVAL_ID}-evaluate-file \
-        --output=output/evaluate/evaluate_proj_${proj_idx}_${file_idx}.out \
+        --output=output/evaluate/${EVAL_ID}/evaluate_proj_${proj_idx}_${file_idx}.out \
         $TT_DIR/swarm/evaluate-proj.sh ${EVAL_ID} --proj_idx ${proj_idx} --file_idx ${file_idx} "$@"
   fi
 done
