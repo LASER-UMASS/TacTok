b1105c141f609c7d72f081d27e84dec889f85b92
Alex Sanchez-Stern <sanchezstern@cs.umass.edu>
2021-11-07 20:34:37 -0500

    Allow monitoring multiple eval ids in show-tasks-left

--- swarm/show-tasks-left.sh
+++ swarm/show-tasks-left.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 SFLAGS="-u $USER -h"
-if [[ $# -gt 0 ]]; then
-  SFLAGS+=" -n $1-evaluate-file,$1-evaluate-proof"
-fi
+#if [[ $# -gt 0 ]]; then
+#    SFLAGS+=" -n $1-evaluate-file,$1-evaluate-proof"
+#fi
 
-NUM_LEFT=$(squeue $SFLAGS | wc -l)
-echo -n $'\r'${NUM_LEFT}'  '
-while [[ ${NUM_LEFT} -gt 0 ]]; do
-    NUM_LEFT=$(squeue $SFLAGS | wc -l)
-    echo -n $'\r'${NUM_LEFT}'  '
+while 
+    if [[ $# -gt 0 ]]; then
+        TOTAL=0
+        for i in "$@"; do
+            NUM_LEFT=$(squeue $SFLAGS -n $i-evaluate-file,$i-evaluate-proof | wc -l)
+            ((TOTAL+=$NUM_LEFT))
+            echo -n '  '$NUM_LEFT'  '
+        done
+        echo -n $'\r'
+    else
+        NUM_LEFT=$(squeue $SFLAGS | wc -l)
+        TOTAL=$NUM_LEFT
+        echo -n $'\r'${NUM_LEFT}'  '
+    fi
     sleep 0.1
-done
+    [[ ${TOTAL} -gt 0 ]]
+do true; done
+echo ""
