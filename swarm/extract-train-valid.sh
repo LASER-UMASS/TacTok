8a3622d4dad0e544fdeca925199990166cfe8954
Alex Sanchez-Stern <sanchezstern@cs.umass.edu>
2021-11-16 13:51:39 -0500

    Update step extraction script

--- swarm/extract-train-valid.sh
+++ swarm/extract-train-valid.sh
@@ -6,15 +6,10 @@ PS=$TT_DIR/projs_split.json
 NUM_PROJS=$(( $(jq ".projs_train | length" $PS) + $(jq ".projs_valid | length" $PS) ))
 mkdir -p output/extract/
 for proj in $(eval echo "{1..$NUM_PROJS}"); do 
-  sbatch --output output/extract/extract_steps_${proj}.out -p longq $TT_DIR/swarm/extract-steps.sh $proj
+  sbatch --output output/extract/extract_steps_${proj}.out -p longq $TT_DIR/swarm/extract-steps-train-valid.sh $proj
 done
 
-while :
-do
-    sleep 1
-    if ! squeue | grep "extract" > /dev/null; then
-        break
-    fi
-done
-cd work/TacTok/TacTok
+$TT_DIR/swarm/show-tasks-left.sh
+
+cd $TT_DIR/TacTok
 python process_proof_steps.py
