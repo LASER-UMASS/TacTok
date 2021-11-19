733c666be1cd4b0519ed9813b8e80da6e61d9c05
Alex Sanchez-Stern <sanchezstern@cs.umass.edu>
2021-11-19 10:09:56 -0500

    Fix vocab size mismatch when merged

--- TacTok/evaluate.py
+++ TacTok/evaluate.py
@@ -65,11 +65,13 @@ if __name__ == '__main__':
 	# The local variable vocabulary (same)
     if opts.include_locals:
         vocab += list(pickle.load(open(opts.local_vocab_file, 'rb')).keys())
-        vocab += ['<unk-local>']
+        if not opts.merge_vocab:
+            vocab += ['<unk-local>']
 
-    if opts.include_paths:
+    if opts.include_paths and not opts.merge_vocab:
         vocab += list(pickle.load(open(opts.path_vocab_file, 'rb')).keys())
-        vocab += ['<unk-path>']
+        if not opts.merge_vocab:
+            vocab += ['<unk-path>']
 
     opts.vocab = vocab
     projs_test = ["weak-up-to", "buchberger", "jordan-curve-theorem", "dblib", "disel", "zchinese", "zfc", "dep-map", "chinese", "UnifySL", "hoare-tut", "huffman", "PolTac", "angles", "coq-procrastination", "coq-library-undecidability", "tree-automata", "coquelicot", "fermat4", "demos", "coqoban", "goedel", "verdi-raft", "verdi", "zorns-lemma", "coqrel", "fundamental-arithmetics"]
