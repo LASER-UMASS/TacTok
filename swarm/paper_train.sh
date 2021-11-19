0223f0ab0dbd690ce9a83e809c6d214917294290
Alex Sanchez-Stern <sanchezstern@gypsum.cm.cluster>
2021-11-19 18:57:46 -0500

    Fix up some scripts

--- swarm/paper_train.sh
+++ swarm/paper_train.sh
@@ -2,35 +2,39 @@
 
 TT_DIR=$HOME/TacTok
 
-FLAGS_DEFAULT="--num-epochs=3 --no-locals-file --max-ident-chunks=4 --bpe-merges=4096"
+FLAGS_DEFAULT="--num_epochs=3 --no-locals-file --max-ident-chunks=4 --bpe-merges=4096"
 
-train-experiment () (
+mkdir -p $TT_DIR/output/paper-evaluate
+
+train-experiment() (
     QUEUE=$1
     EVAL_ID=$2
     shift 2
-    sbatch -p $QUEUE --gres=gpu:1 --mem=8000 --output=$TT_DIR/output/paper-train/$EVAL_ID \
-           -J $EVAL_ID $TT_DIR/swarm/basic_train.sh \
+    sbatch -p $QUEUE --gres=gpu:1 -J $EVAL_ID \
+           --mem=8000 \
+           --output=$TT_DIR/output/paper-train/${EVAL_ID}.out \
+           $TT_DIR/swarm/basic_train.sh \
            --exp_id=$EVAL_ID ${FLAGS_DEFAULT} "$@"
 )
 
 ## MAIN EXPERIMENT ##
 
 # ASTactic + I
-train-experiment titanx-long paper-as+i \
+train-experiment rtx8000-long paper-as+i \
        --no_prev_tactics
 
 # Tac + I
-train-experiment titanx-long paper-tac+i \
+train-experiment rtx8000-long paper-tac+i \
        --tac_vocab_file=tac_vocab_no_unk.pickle \
        --cutoff_len=3
 
 # Tok + I
-train-experiment titanx-longn paper-tok+i
+train-experiment rtx8000-long paper-tok+i
 
 ## TECHNIQUES EXPERIMENT ##
 
 # Tok + Paths
-train-experiment titanx-long paper-tok+paths \
+train-experiment m40-long paper-tok+paths \
        --max-ident-chunks=0 --no_defs --no_locals --no_constructors
 
 # Tok + Constructors
@@ -38,17 +42,17 @@ train-experiment titanx-long paper-tok+cons \
       --max-ident-chunks=0 --no_defs --no_locals --no_paths
 
 # Tok + Globals
-train-experiment titanx-long paper-tok+globals \
+train-experiment m40-long paper-tok+globals \
        --max-ident-chunks=0 --no_locals --no_paths --no_constructors
 
 # Tok + Locals
-train-experiment titanx-long paper-tok+locals \
+train-experiment m40-long paper-tok+locals \
        --max-ident-chunks=0 --no_defs --no_paths --no_constructors
 
 ## BPE EXPERIMENT ##
 
 # Tok + Paths + BPE
-train-experiment titanx-long paper-tok+paths+bpe \
+train-experiment 2080ti-long paper-tok+paths+bpe \
        --no_defs --no_locals --no_constructors
 
 # Tok + Constructors + BPE
@@ -56,11 +60,11 @@ train-experiment titanx-long paper-tok+cons+bpe \
        --no_defs --no_locals --no_paths
 
 # Tok + Globals
-train-experiment titanx-long paper-tok+globals+bpe \
+train-experiment 2080ti-long paper-tok+globals+bpe \
        --no_locals --no_paths --no_constructors
 
 # Tok + Locals
-train-experiment titanx-long paper-tok+locals+bpe \
+train-experiment 2080ti-long paper-tok+locals+bpe \
        --no_defs --no_paths --no_constructors
 
 
@@ -69,21 +73,21 @@ train-experiment titanx-long paper-tok+merged \
        --merge_vocab
 
 ## GLOBAL VOCAB SIZE ##
-for vocab_size in {20..900..10}; do
+for vocab_size in {20..900..20}; do
     train-experiment 1080ti-long paper-globals-vsize-${vocab_size} \
-           --def_vocab_file='./names/names-known-${vocab_size}.pickle'
+           --def_vocab_file="./names/names-known-${vocab_size}.pickle"
 done
 
 ## LOCAL VOCAB SIZE ##
-for vocab_size in {20..900..10}; do
-    train-experiment 1080ti-long paper-locals-vsize-${vocab_size} \
-           --local_vocab_file='./names/locals-known-${vocab_size}.pickle'
+for vocab_size in {20..900..20}; do
+    train-experiment titanx-long paper-locals-vsize-${vocab_size} \
+           --local_vocab_file="./names/locals-known-${vocab_size}.pickle"
 done
 
 ## PATH VOCAB SIZE ##
-for vocab_size in {20..900..10}; do
-    train-experiment 2080ti-long paper-paths-vsize-${vocab_size}
-           --path_vocab_file='./names/paths-known-${vocab_size}.pickle'
+for vocab_size in {20..900..20}; do
+    train-experiment 2080ti-long paper-paths-vsize-${vocab_size} \
+           --path_vocab_file="./names/paths-known-${vocab_size}.pickle"
 done
 
 ## CONSTRUCTOR VOCAB SIZE ##
