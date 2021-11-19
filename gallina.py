5025ec23b0b24f420071c9956e800a2f596dcd4a
Alex Sanchez-Stern <sanchezstern@cs.umass.edu>
2021-11-19 11:23:15 -0500

    Fix typo in gallina.py

--- gallina.py
+++ gallina.py
@@ -65,7 +65,7 @@ class GallinaTermParser:
                 # Don't erase fully-qualified definition & theorem names
                 elif ((syn_conf.include_defs and SyntaxConfig.is_label(node)) or
                 (syn_conf.include_paths and SyntaxConfig.is_path(node)) or
-                (syn_conf.merge_vocab and syn_config.include_locals and SyntaxConfig.is_local(node))):
+                (syn_conf.merge_vocab and syn_conf.include_locals and SyntaxConfig.is_local(node))):
                     ident_wrapper = SyntaxConfig.singleton_ident(c.value)
                     node.height = 2
                     children.append(ident_wrapper)
