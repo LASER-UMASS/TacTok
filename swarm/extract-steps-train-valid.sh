32398bb1b060ae248a96612feb056c2c717333c4
Alex Sanchez-Stern <sanchezstern@cs.umass.edu>
2021-12-16 14:27:21 -0500

    Allow passing arguments through extract-steps-train-valid.sh

--- swarm/extract-steps-train-valid.sh
+++ swarm/extract-steps-train-valid.sh
@@ -4,10 +4,11 @@ TT_DIR=$HOME/work/TacTok/
 
 set -ex
 
-[[ "$#" -ne 1 ]] && echo "Wrong number of parameters! This script takes one argument, a project index" && exit 1
+[[ "$#" -lt 1 ]] && echo "Wrong number of parameters! This script takes one argument, a project index" && exit 1
 
 source ${TT_DIR}/swarm/prelude.sh
 
 PROJ=$(cat <(jq -r ".projs_train[]" projs_split.json) <(jq -r ".projs_valid[]" projs_split.json) | awk "NR==$1")
+shift 1
 cd TacTok
-python extract_proof_steps.py --data_root "../data/$PROJ"
+python extract_proof_steps.py --data_root "../data/$PROJ" $@
