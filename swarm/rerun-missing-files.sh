40764eb942dfe573a65e4745c5ddf643023f7b97
Alex Sanchez-Stern <sanchezstern@cs.umass.edu>
2022-01-07 15:33:04 -0500

    Use defq instead of longq for running individual files

--- swarm/rerun-missing-files.sh
+++ swarm/rerun-missing-files.sh
@@ -13,7 +13,7 @@ mkdir -p output/evaluate/${EVAL_ID}
 $TT_DIR/swarm/find-missing-outputs-csv.sh ${TT_DIR}/TacTok/evaluation/${EVAL_ID} |
 while IFS=, read -r proj_idx proj_name file_idx file_name proof_idx proof_name; do
   if [[ $proof_idx == "" ]]; then
-      sbatch -p longq -J ${EVAL_ID}-evaluate-file \
+      sbatch -p defq -J ${EVAL_ID}-evaluate-file \
         --output=output/evaluate/${EVAL_ID}/evaluate_proj_${proj_idx}_${file_idx}.out \
         $TT_DIR/swarm/evaluate-proj.sh ${EVAL_ID} --proj_idx ${proj_idx} --file_idx ${file_idx} "$@"
   fi
