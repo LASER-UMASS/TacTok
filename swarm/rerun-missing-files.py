b5f4e0a3537001173d2692ec826a643cfd0cdb92
Alex Sanchez-Stern <sanchezstern@cs.umass.edu>
2022-01-07 18:41:21 -0500

    Make rerun-missing-files start running batches while streaming missing file info

--- swarm/rerun-missing-files.py
+++ swarm/rerun-missing-files.py
@@ -4,6 +4,7 @@ import subprocess
 import argparse
 import csv
 import os
+import io
 from os.path import expandvars
 
 parser = argparse.ArgumentParser()
@@ -11,25 +12,36 @@ parser.add_argument("eval_id")
 args, rest_args = parser.parse_known_args()
 tt_dir = expandvars("$HOME/work/TacTok")
 
-result = subprocess.run([f"{tt_dir}/swarm/find-missing-outputs-csv.sh", 
-                         f"{tt_dir}/TacTok/evaluation/{args.eval_id}"],
-                        capture_output=True, text=True)
+result = subprocess.Popen([f"{tt_dir}/swarm/find-missing-outputs-csv.sh",
+                           f"{tt_dir}/TacTok/evaluation/{args.eval_id}"],
+                          stdout=subprocess.PIPE, text=True)
 
-csvreader = csv.reader(iter(result.stdout.splitlines()))
+csvreader = csv.reader(result.stdout)
 proj_files = {}
+last_proj_idx = None
 for row in csvreader:
     proj_idx, proj_name, file_idx, file_name, proof_idx, proof_name = row
     if proof_idx != "":
         continue
+    if last_proj_idx and proj_idx != last_proj_idx:
+        file_idxs = proj_files[last_proj_idx]
+        subprocess.run(["sbatch",
+                        "-p", "defq",
+                        "-J", f"{args.eval_id}-evaluate-file",
+                        f"--output={tt_dir}/output/evaluate/{args.eval_id}/evaluate_proj_{last_proj_idx}_%a.out",
+                        f"--array={','.join(file_idxs)}",
+                        f"{tt_dir}/swarm/evaluate-proj-array-item.sbatch",
+                        args.eval_id, proj_idx] + rest_args)
+    last_proj_idx = proj_idx
     if proj_idx not in proj_files:
         proj_files[proj_idx] = []
     proj_files[proj_idx].append(file_idx)
 
-for proj_idx, file_idxs in proj_files.items():
-    subprocess.run(["sbatch",
-                    "-p", "defq",
-                    "-J", f"{args.eval_id}-evaluate-file",
-                    f"--output={tt_dir}/output/evaluate/{args.eval_id}/evaluate_proj_{proj_idx}_%a.out",
-                    f"--array={','.join(file_idxs)}",
-                    f"{tt_dir}/swarm/evaluate-proj-array-item.sbatch",
-                    args.eval_id, proj_idx] + rest_args)
+file_idxs = proj_files[last_proj_idx]
+subprocess.run(["sbatch",
+                "-p", "defq",
+                "-J", f"{args.eval_id}-evaluate-file",
+                f"--output={tt_dir}/output/evaluate/{args.eval_id}/evaluate_proj_{last_proj_idx}_%a.out",
+                f"--array={','.join(file_idxs)}",
+                f"{tt_dir}/swarm/evaluate-proj-array-item.sbatch",
+                args.eval_id, last_proj_idx] + rest_args)
