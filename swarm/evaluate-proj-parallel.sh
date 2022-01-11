b807b9dc821e32825edccad4edef859f642565cb
Alex Sanchez-Stern <sanchezstern@cs.umass.edu>
2022-01-11 13:16:46 -0500

    Add missing semicolon

--- swarm/evaluate-proj-parallel.sh
+++ swarm/evaluate-proj-parallel.sh
@@ -7,7 +7,7 @@ PROJ_IDX=$2
 shift 2
 PROJ=$(jq -r ".projs_test[]" ${TT_DIR}/projs_split.json | awk "NR==($PROJ_IDX+1)")
 NUM_FILES=$(find ${TT_DIR}/data/${PROJ} -name "*.json" | wc -l)
-if (( $NUM_FILES == 0 )); then exit 0 fi
+if (( $NUM_FILES == 0 )); then exit 0; fi
 
 mkdir -p output/evaluate/${EVAL_ID}
 $TT_DIR/swarm/sbatch-retry.sh -J ${EVAL_ID}-evaluate-file -p longq \
