bd76db9249235fda41ff072e94f02c8850ea67e9
Alex Sanchez-Stern <sanchezstern@cs.umass.edu>
2022-01-12 10:55:48 -0500

    Sleep before checking results for retry

--- swarm/sbatch-retry.sh
+++ swarm/sbatch-retry.sh
@@ -16,13 +16,13 @@ done
 JOBS_BEFORE=$($TT_DIR/swarm/squeue-retry.sh $SFLAGS)
 while
     sbatch $@
+    /usr/bin/env sleep $BACKOFF_AMOUNT
     JOBS_AFTER=$($TT_DIR/swarm/squeue-retry.sh $SFLAGS)
     diff <(echo "$JOBS_BEFORE" | awk '{print $1}' | sort) \
          <(echo "$JOBS_AFTER"  | awk '{print $1}' | sort) | grep -q "> "
     (( $? != 0 ))
 do
     echo "Submission failed, retrying with delay ${BACKOFF_AMOUNT}s..." >&2
-    /usr/bin/env sleep $BACKOFF_AMOUNT
     BACKOFF_AMOUNT=$(echo "$BACKOFF_AMOUNT * 2" | bc)
     JOBS_BEFORE="$JOBS_AFTER"
 done
