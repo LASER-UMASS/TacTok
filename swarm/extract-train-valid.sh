371c48812a8fa4f97d9da099ab70c8c042c34e70
Alex Sanchez-Stern <sanchezstern@cs.umass.edu>
2022-01-25 11:19:46 -0500

    Add TacTok features to steps on initial extraction pass

--- swarm/extract-train-valid.sh
+++ swarm/extract-train-valid.sh
@@ -41,20 +41,3 @@ if [ -d $DEST ]; then
         echo "Aborting..." && exit 1 ;;
     esac
 fi
-
-for proj_idx in $(eval echo "{1..$NUM_PROJS}"); do
-  PROJ=$(cat <(jq -r ".projs_train[]" ${TT_DIR}/projs_split.json) \
-             <(jq -r ".projs_valid[]" ${TT_DIR}/projs_split.json) \
-             | awk "NR==$proj_idx")
-  NUM_FILES=$(find ${TT_DIR}/data/${PROJ} -name "*.json" | wc -l)
-  if [[ $NUM_FILES -eq 0 ]]; then
-    continue
-  fi
-  sbatch --output output/extract/extract_steps_${PROJ}_%a.out -p longq --array=0-$(($NUM_FILES - 1 )) \
-    $TT_DIR/swarm/extract-steps-train-valid.sh $PROJ --output=${DEST}
-done
-
-$TT_DIR/swarm/show-tasks-left.sh -b
-
-cd $TT_DIR/TacTok
-python process_proof_steps.py
