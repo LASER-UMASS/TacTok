a705d59b9510e939e5826394e1479a5173e4f15f
Alex Sanchez-Stern <sanchezstern@cs.umass.edu>
2022-01-25 20:54:03 -0500

    Fix some makefiles

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ COQ_ROOT=$(pwd)/coq
 DEPENDENCIES="dune=1.10.0 cmdliner=1.0.4 ppx_sexp_conv=v0.12.0 ppx_deriving=4.3 sexplib=v0.12.0 ppx_import=1.6.2 camlp5=7.08 coq=8.9.1"
 
 echo "Installing Dependencies.."
-opam switch $OPAM_SWITCH && eval $(opam env)
-opam install --yes $DEPENDENCIES
+write-opam.sh switch $OPAM_SWITCH && eval $(opam env)
+write-opam.sh install --yes $DEPENDENCIES
 echo "Dependencies installed"
 
 echo "Installing Coq.."
