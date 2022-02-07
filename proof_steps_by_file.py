c77ace499a6d8c353855cf6652882233915b2934
Alex Sanchez-Stern <sanchezstern@cs.umass.edu>
2022-02-07 14:08:28 -0500

    Add --to-hashes option to proof_steps_by_file.py

--- proof_steps_by_file.py
+++ proof_steps_by_file.py
@@ -13,6 +13,8 @@ from tqdm import tqdm
 import fcntl
 import time
 from lark import Tree
+from typing import Any, Dict
+from hashlib import sha256
 
 class LarkEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -21,6 +23,11 @@ class LarkEncoder(json.JSONEncoder):
                     "data": obj.data,
                     "children": [self.default(child) for child in obj.children]}
         return json.JSONEncoder.default(self, obj) 
+
+def lark_decoder(dct):
+     if "__is_tree__" in dct:
+         return Tree(dct["data"], dct["children"])
+     return dct
 
 class FileLock:
     def __init__(self, file_handle):
@@ -38,10 +45,15 @@ class FileLock:
     def __exit__(self, type, value, traceback):
         fcntl.flock(self.file_handle, fcntl.LOCK_UN)
 
-def get_local_path(file):
-    parts = Path(file).parts
+def get_local_path(filename):
+    parts = Path(filename).parts
     i = parts.index('data')
     return os.path.join(*parts[i + 1:])
+
+def get_proj(filename):
+    parts = Path(filename).parts
+    i = parts.index('data')
+    return parts[i+1]
 
 def to_file(args: argparse.Namespace, filename: str) -> None:
     with open(filename, 'rb') as f:
@@ -52,13 +64,35 @@ def to_file(args: argparse.Namespace, filename: str) -> None:
     with open(outpath, 'a') as f, FileLock(f):
         print(json.dumps(step, cls=LarkEncoder), file=f)
 
+def to_hashes(args: argparse.Namespace, projs_split: Dict[str, List[str]], filename: str) -> None:
+    with open(filename, 'r') as f:
+        for line in f:
+            step = json.loads(line, object_hook=lark_decoder)
+            step_hash = sha256(repr(step).encode('utf-8')).hexdigest()
+            if get_proj(step["file"]) in projs_split["projs_train"]:
+                outpath = os.path.join(args.outdir, "train", f"{stephash}.pickle")
+            else:
+                assert get_proj(step["file"]) in projs_split("projs_valid"), get_proj(step["file"])
+                outpath = os.path.join(args.outdir, "valid", f"{stephash}.pickle")
+
+            os.makedirs(os.path.dirname(outpath), exist_ok=True)
+            with open(outpath, 'w') as f:
+                pickle.dump(step, outpath)
+
 parser = argparse.ArgumentParser()
-parser.add_argument("stepsdir")
+parser.add_argument("indir")
 parser.add_argument("outdir")
 parser.add_argument("-j", "--num-threads", default=4)
+parser.add_argument("--to-hashes", action="store_true")
+parser.add_argument("--projs_split", default="projs_split.json")
 args = parser.parse_args()
 
+paths = glob.glob(os.path.join(args.indir, "**/*", recursive=True))
 with multiprocessing.Pool(args.num_threads) as pool:
-    paths = glob.glob(os.path.join(args.stepsdir, "**/*"))
-    res = list(tqdm(pool.imap(functools.partial(to_file, args), paths), total=len(paths)))
+    if args.to_hashes:
+        projs_split = json.load(open(args.projs_split))
+        res = list(tqdm(pool.imap(functools.partial(to_hashes, args, projs_split), paths), 
+                        total=len(paths)))
+    else:
+        res = list(tqdm(pool.imap(functools.partial(to_file, args), paths), total=len(paths)))
   
