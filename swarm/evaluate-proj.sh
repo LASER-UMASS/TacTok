9f3cd484a622b8956d781acd815e2dbc7cc66139
timothytmzhou <timothytmzhou@gmail.com>
2022-02-09 17:28:11 -0600

    add missing OUTDIR definition in evaluate-proj

--- swarm/evaluate-proj.sh
+++ swarm/evaluate-proj.sh
@@ -12,6 +12,8 @@ EVAL_ID=$1
 PROJ_IDX=$2
 shift 1
 
+OUTDIR=$TT_DIR/TacTok/evaluation/${EVAL_ID}/
+
 cd TacTok
 if [ $PROJ_IDX -eq 0 ]; then
   python evaluate.py ours ${EVAL_ID} --path runs/${EVAL_ID}/checkpoints/model_002.pth --export_config=${OUTDIR}/flags.json "$@"
