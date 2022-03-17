be85fc4dc6a6fe6114c6ec49dd4a2c51078d281e
Alex Sanchez-Stern <sanchezstern@cs.umass.edu>
2022-03-17 10:10:46 -0400

    Remove unecessary imports

--- TacTok/process_results.py
+++ TacTok/process_results.py
@@ -9,13 +9,7 @@
 # counts.
 
 import json
-import os
-import sys
 from glob import glob
-import numpy as np
-import pickle
-import random
-import itertools 
 from args import ConfigParser
 
 projs = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,16,17,18,19,20,21,22,23,24,25,26]
